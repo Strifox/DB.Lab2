0d24e1e58c8c41803b719f4b8e63808ca61d36ca
Johan Svensson <johan.svensson93@hotmail.se>
2017-12-07 14:19:06 +0100

    gg

---
+++ DB.Lab2/Data/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DB.Lab2
+{
+    class Menu
+    {
+        public void MenuDatabase()
+        {
+            Console.WriteLine("Welcome to Angry Birds Console");
+            Thread.Sleep(5000);
+            Console.Clear();
+
+            Console.WriteLine("Press '1' to Add a player");
+        }
+    }
+}

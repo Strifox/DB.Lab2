b74200bf0b78c094663333fceffff2899888f9dc
Johan Svensson <johan.svensson93@hotmail.se>
2018-01-11 14:11:49 +0100

    Map class changed. Added new if statement and updated database

--- DB.Lab2/Data/Entities/Map.cs
+++ DB.Lab2/Data/Entities/Map.cs
@@ -16,23 +16,49 @@ namespace DB.Lab2
         [Column("Moves", TypeName = "int")]
         public int Moves { get; set; }
 
-        public Map(int moves)
+        [Column("MapName", TypeName = "nvarchar")]
+        [MaxLength(50)]
+        public string MapName { get; set; }
+
+        public Map(int moves, string mapName)
         {
+            MapName = mapName;
             Moves = moves;
         }
 
         public Map()
         {
-            
+
         }
 
         public void AddMapToDatabase(EntityContext context)
         {
+            Console.WriteLine("Enter a map name");
+            MapName = Console.ReadLine();
             Console.WriteLine("Enter Max amount of moves");
             Moves = int.Parse(Console.ReadLine());
-            context.Maps.Add(new Map(Moves));
+            context.Maps.Add(new Map(Moves, MapName));
             context.SaveChanges();
+            Console.WriteLine($"{MapName} added to database");
         }
 
+        public void IsMapAdded(EntityContext context)
+        {
+            if (!context.Maps.Any())
+                Console.WriteLine("You need to add an map");
+
+            AddMapToDatabase(context);
+
+            var mapQuery = from map in context.Maps
+                           select map.MapName;
+
+            foreach (var mapName in mapQuery)
+            {
+                if (mapName == MapName)
+                    Console.WriteLine("The map name already exists");
+                else
+                   AddMapToDatabase(context);
+            }
+        }
     }
 }
--- DB.Lab2/Data/Menu.cs
+++ DB.Lab2/Data/Menu.cs
@@ -17,7 +17,7 @@ namespace DB.Lab2
         public void MenuDatabase()
         {
             Console.WriteLine("Welcome to Angry Birds Console");
-            Thread.Sleep(5000);
+            Thread.Sleep(2000);
             Console.Clear();
 
             Console.WriteLine("Press '1' to Add a player");
@@ -34,7 +34,7 @@ namespace DB.Lab2
             switch (menuChoice)
             {
                 case "1":
-                    m.AddMapToDatabase(e);
+                    m.IsMapAdded(e);
                     p.AddPlayerToDatabase(e);
                     break;
                 case "2":
